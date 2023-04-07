cec6419a28ed63d15d89a5c3305e086e8f8233dc
Cyberghxst <jeffreyvillatoroquezada@gmail.com>
2023-04-06 23:10:56 -0600

    clean up

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { ErineClient } from "./classes/Base";
-import { PrefixBuilder } from "./classes/builders/PrefixBuilder";
-import { HybridBuilder } from "./classes/builders/HybridBuilder";
-import { ParamsBuilder } from "./classes/builders/ParamsBuilder";
-import { Plugins } from "./classes/Core";
-import * as Errors from "./classes/Errors"
-
-export { ErineClient, PrefixBuilder, HybridBuilder, ParamsBuilder, Errors, Plugins }
